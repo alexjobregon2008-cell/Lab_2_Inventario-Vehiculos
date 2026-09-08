fcb78d9cd05dfdbae7d9c83d9cd9e3735b9d7971
Alex Obregón <alexobregon@gmail.com>
2026-09-08 00:06:28 -0500

    Se Agrega el Main del Proyecto

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import datos.LectorVehiculos;
+import modelo.Vehiculo;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        LectorVehiculos lector = new LectorVehiculos();
+        Path rutaArchivo = Path.of("src", "vehiculos.csv");
+        try {
+            List<Vehiculo> vehiculos = lector.cargar(rutaArchivo.toString());
+            System.out.println("=== INVENTARIO DE VEHÍCULOS ===");
+            for (Vehiculo vehiculo : vehiculos) {
+                System.out.println(vehiculo.mostrarInformacion());
+            }
+            System.out.println("Total de vehículos: " + vehiculos.size());
+        } catch (IOException | NumberFormatException e) {
+            System.out.println("No fue posible leer el inventario: " + e.getMessage());
+        }
     }
 }
